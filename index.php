239c71ed527658d781dd4c7085d6335bb06af5d2
Lucas Alves <codestep@codingstep.com.br>
2022-09-14 16:55:12 -0300

    fixed bug

--- index.php
+++ index.php
@@ -5,11 +5,12 @@ declare(strict_types=1);
 require_once 'vendor/autoload.php';
 
 use Codestep\HolyBible\Bible;
+use Codestep\HolyBible\Books;
 
 $bible = Bible::getInstance();
 
 try {
-    print_r($bible->getChapter());
+    print_r($bible->getVerse(Books::GENESIS, 1, 5));
 } catch (Exception $e) {
     die($e->getMessage());
 }
---
+++ tests/BibleTest.php
@@ -0,0 +1,48 @@
+<?php
+
+
+use PHPUnit\Framework\TestCase;
+
+class BibleTest extends TestCase
+{
+
+    public function testGetVersions()
+    {
+
+    }
+
+    public function testGetChapter()
+    {
+
+    }
+
+    public function testSetUserToken()
+    {
+
+    }
+
+    public function testGetUserToken()
+    {
+
+    }
+
+    public function testGetVerse()
+    {
+
+    }
+
+    public function testSetVersion()
+    {
+
+    }
+
+    public function testGetBooks()
+    {
+
+    }
+
+    public function testGetVersion()
+    {
+
+    }
+}
